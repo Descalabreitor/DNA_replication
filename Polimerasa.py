994d437bd21b306895c65e7b7d62b4848d7010ec
Alvaroooooooo <90764257+Alvaroooooooo@users.noreply.github.com>
2023-10-03 13:48:40 +0100

    polimerasa

--- Polimerasa.py
+++ Polimerasa.py
@@ -8,3 +8,20 @@ class Polimerasa:
         complemento = {"A": "T", "T": "A", "C": "G", "G": "C"}
         new_strand = ''.join(complemento[base] for base in template_strand)
         return new_strand
+
+    def mitosis(self, helice):
+        new_helice_1 = helice[0] + helice[1]
+        new_helice_2 = helice[1] + helice[0]
+        return new_helice_1, new_helice_2
+
+    def meiosis_1(self, helice):
+        len_helice = len(helice[0])
+        new_helice_1 = helice[0][:len_helice // 2] + helice[1][:len_helice // 2]
+        new_helice_2 = helice[1][:len_helice // 2] + helice[0][:len_helice // 2]
+        return new_helice_1, new_helice_2
+
+    def meiosis_2(self, helice):
+        len_helice = len(helice[0])
+        new_helice_1 = helice[0][len_helice // 2:] + helice[1][len_helice // 2:]
+        new_helice_2 = helice[1][len_helice // 2:] + helice[0][len_helice // 2:]
+        return new_helice_1, new_helice_2
