16b47910186ecc8b5bd3d3953afc5710dfaa03a9
Alvaroooooooo <90764257+Alvaroooooooo@users.noreply.github.com>
2023-10-04 19:58:39 +0100

    meiosis

--- Main.py
+++ Main.py
@@ -11,17 +11,31 @@ def main():
     # Crear una instancia de Nucleus con la lista de ADN
     nucleus = Nucleus(dna_list)
 
-    # Replicar el ADN en el núcleo
-    nucleus1, nucleus2 = nucleus.replicate_dna()
+    # Replicar el ADN en el núcleo mediante mitosis
+    nucleus1_mitosis, nucleus2_mitosis = nucleus.replicate_dna(meiosis=False)
 
-    # Imprimir las hebras de ADN resultantes
-    print("Hebras de ADN en Núcleo 1:")
-    for dna in nucleus1.dna_list:
+    # Imprimir las hebras de ADN resultantes de la mitosis
+    print("Mitosis - Hebras de ADN en Núcleo 1:")
+    for dna in nucleus1_mitosis.dna_list:
         print(dna.get_helice())
 
-    print("\nHebras de ADN en Núcleo 2:")
-    for dna in nucleus2.dna_list:
+    print("\nMitosis - Hebras de ADN en Núcleo 2:")
+    for dna in nucleus2_mitosis.dna_list:
+        print(dna.get_helice())
+
+    # Replicar el ADN en el núcleo mediante meiosis
+    nucleus1_meiosis, nucleus2_meiosis = nucleus.replicate_dna(meiosis=True)
+
+    # Imprimir las hebras de ADN resultantes de la meiosis
+    print("\nMeiosis - Hebras de ADN en Núcleo 1:")
+    for dna in nucleus1_meiosis.dna_list:
+        print(dna.get_helice())
+
+    print("\nMeiosis - Hebras de ADN en Núcleo 2:")
+    for dna in nucleus2_meiosis.dna_list:
         print(dna.get_helice())
 
 if __name__ == "__main__":
     main()
+
+
--- Nucleus.py
+++ Nucleus.py
@@ -11,14 +11,20 @@ class Nucleus:
             self.dna_list.extend(dna_gen.gen_data())
 
 
-    def replicate_dna(self):
+    def replicate_dna(self, meiosis=False):
         polimerasa = Polimerasa()
         dna_list_1 = []
         dna_list_2 = []
         
         for dna in self.dna_list:
-            new_dna1, new_dna2 = polimerasa.replicate(dna.get_helice())
-            dna_list_1.append(new_dna1)
-            dna_list_2.append(new_dna2)
+            if meiosis:
+                new_dna1, new_dna2 = polimerasa.meiosis_1(dna.get_helice())
+                new_dna3, new_dna4 = polimerasa.meiosis_2(dna.get_helice())
+                dna_list_1.append(DNA(''.join([new_dna1, new_dna2])))
+                dna_list_2.append(DNA(''.join([new_dna3, new_dna4])))
+            else:
+                new_dna1, new_dna2 = polimerasa.replicate(dna.get_helice())
+                dna_list_1.append(DNA(''.join([new_dna1, new_dna2])))
+                dna_list_2.append(DNA(''.join([new_dna1, new_dna2])))
 
         return Nucleus(dna_list_1), Nucleus(dna_list_2)
